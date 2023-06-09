7bcfc412540df440a91ee771a8d71343dd4e524c
zamaraev vyacheslav <slavazworld@mail.ru>
2023-06-09 17:41:22 +0500

    final1

--- Program.cs
+++ Program.cs
@@ -7,31 +7,29 @@ class Program
         Console.Write(promt);
         return int.Parse(Console.ReadLine());
     }
-    static double ReadDouble(string promt)  // Метод для вещественных чисел
-    {
-        Console.Write(promt);
-        return double.Parse(Console.ReadLine());
-    }
-
+   
     static void Main(string[] args)
     {
 
-        double start = ReadDouble("Введите начало промежутка:  "); 
-        double end =   ReadDouble("Введите конец промежутка :  ");
-        double step = ReadDouble("Введите шаг               :  ");
+        int start = ReadInt ("Введите число А: "); 
+        int end =   ReadInt("Введите число В (A<B):  ");
+      
+        Console.WriteLine("+---------+--------------------+");    
+        Console.WriteLine("|  Число  |  Кол-во делителей  |");
+        Console.WriteLine("+---------+--------------------+");
+        
+        uint divider = 0;
+        for (int x = start; x<= end; x += 1){
+            for(int i = 1;  i<=x; i++)
+            {
+                if(x%i == 0)
+                divider++;
+            }
+            //uint divider = 0;   // Находим делитель
 
-        Console.WriteLine("+---------+--------------+--------------+");    
-        Console.WriteLine("|    x    |    sin(x)    |    cos(x)    |");
-        Console.WriteLine("+---------+--------------+--------------+");
-        //Console.WriteLine();
-       // Console.WriteLine();
-        //Console.WriteLine();
-        for (double x = start; x<= end; x += step){
-            double y1 = Math.Sin(x);
             double y2 = Math.Cos(x);
-            Console.WriteLine("| {0,7:F3} | {1,12:F4} | {2,12:F4} |",x,y1,y2);     
+            Console.WriteLine("| {0,7:F3} | {1,18:F4} |",x,divider);     
         }
-        Console.WriteLine("+---------+--------------+--------------+");
+        Console.WriteLine("+---------+--------------------+");
 
-    }
-}
+    }}
