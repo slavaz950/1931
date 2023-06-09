3188cba41553bd9a53d2a5974891c9e0a470098b
zamaraev vyacheslav <slavazworld@mail.ru>
2023-06-09 13:08:26 +0500

    commit4

--- Program.cs
+++ Program.cs
@@ -1,22 +1,13 @@
 ﻿namespace _1931;
 class Program
-{
-   
-}
 
-
-
-
-namespace Zamaraev_Task3;
-class Program
 {
     static int ReadInt(string promt)  // Метод для целых чисел
     {
         Console.Write(promt);
         return int.Parse(Console.ReadLine());
     }
-
-    static int ReadDouble(string promt)  // Метод для вещественных чисел
+    static double ReadDouble(string promt)  // Метод для вещественных чисел
     {
         Console.Write(promt);
         return double.Parse(Console.ReadLine());
@@ -31,7 +22,7 @@ class Program
 
         for (double x = start; x<= end; x += step){
             double y1 = Math.Sin(x);
-            double y1 = Math.Cos(x);
+            double y2 = Math.Cos(x);
             Console.WriteLine("x={0} sin(x)={1} cos(x)={2}" );
         }
 
